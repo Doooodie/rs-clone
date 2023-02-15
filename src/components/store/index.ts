8075348374b521c9904e5ab09ce6281eccaa9077
LineCoran <vppisya@gmail.com>
2023-02-15 21:30:31 +0300

    feat: add ApiSlice

--- src/components/store/index.ts
+++ src/components/store/index.ts
@@ -20,6 +20,7 @@ const rootReducer = combineReducers({
   files: filesReducer,
   filter: filterReducer,
   fileInfo: fileInfoReducer,
+  [filesApi.reducerPath]: filesApi.reducer,
 });
 
 const persistConfig = {
@@ -30,14 +31,21 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
+const middlewareHandler = (getDefaultMiddleware: any) => {
+  const middlewareList = [
+    ...getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
+    filesApi.middleware,
+  ];
+  return middlewareList;
+};
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => middlewareHandler(getDefaultMiddleware)
 });
 
 export default store;
@@ -46,3 +54,6 @@ export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+import { filesApi } from './../pages/Explorer/api/api';
+
